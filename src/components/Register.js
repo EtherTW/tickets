d08644e3f4646d0a75734c30a3f57189b00d018f
Yuren Ju <yurenju@gmail.com>
2018-03-04 10:03:30 +0800

    Use firebase to store email, username and transaction

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,18 @@
 import Eth from 'ethjs';
-
+import Firebase from 'firebase';
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText, Alert } from 'reactstrap';
 
 const contractAddress = '0xd182db77ac90d77646b895be09e59690e4fa68e1';
 const ABI = JSON.parse('[{ "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "ticket", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "puzzle", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_attendees", "type": "address" }], "name": "getTicket", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_owner", "type": "address" }], "name": "transferOwner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_secret", "type": "bytes" }], "name": "calculatePuzzle", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "DEPOSIT", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_secret", "type": "bytes" }], "name": "getRefund", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_puzzle", "type": "bytes32" }], "name": "setPuzzle", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }]');
+const firebaseConfig = {
+  apiKey: "AIzaSyCIP2W4I3aA9AOuvXpUpQLzFBmwvo3lQy8",
+  authDomain: "taipei-ethereum-meetup-ticket.firebaseapp.com",
+  databaseURL: "https://taipei-ethereum-meetup-ticket.firebaseio.com",
+  projectId: "taipei-ethereum-meetup-ticket",
+  storageBucket: "taipei-ethereum-meetup-ticket.appspot.com",
+  messagingSenderId: "515416889778"
+}
 
 class Register extends Component {
   constructor (props) {
@@ -13,13 +21,20 @@ class Register extends Component {
     this.state = {
       wallet: '',
       transaction: '',
-      hadTicket: false
+      hadTicket: false,
+      metamask: true,
+      name: '',
+      email: ''
     };
 
     this.deposit = 0.001;
   }
 
   async componentDidMount () {
+    // Initial firebase
+    this.firebase = Firebase.initializeApp(firebaseConfig);
+
+    // Initial with metamask
     if (typeof window.web3 !== 'undefined') {
       let eth = new Eth(window.web3.currentProvider);
       const accounts = await eth.accounts();
@@ -30,6 +45,8 @@ class Register extends Component {
         const result = await ticket.ticket(wallet);
         this.setState({ wallet, hadTicket: result[0] });
       }
+    } else {
+      this.setState({metamask: false});
     }
   }
 
@@ -43,7 +60,20 @@ class Register extends Component {
       gasPrice: Eth.toWei(21, 'Gwei'),
       data: '0x'
     });
+    const result = this.firebase.database().ref(`users/${this.state.wallet}`).set({
+      name: this.state.name,
+      email: this.state.email,
+      transaction
+    });
     this.setState({transaction});
+  }
+
+  onNameChange = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value });
   }
 
   renderTransaction () {
@@ -56,6 +86,18 @@ class Register extends Component {
           </Alert>
         </div>
       )
+    }
+  }
+
+  renderError () {
+    if (!this.state.metamask) {
+      return (
+        <div className="my-3">
+          <Alert color="danger">
+            您還沒有安裝 MetaMask，請先到 <a href="https://metamask.io/" target="_blank">官方網站</a> 安裝並且儲值至少約 {this.deposit + 0.001} ETH （押金與給礦工的交易費）。
+          </Alert>
+        </div>
+      );
     }
   }
 
@@ -81,19 +123,20 @@ class Register extends Component {
         <Form className="w-50">
           <FormGroup>
             <Label for="name">名字 / 暱稱</Label>
-            <Input type="text" name="name" id="name" />
+            <Input type="text" name="name" id="name" value={this.state.name} onChange={this.onNameChange} />
           </FormGroup>
           <FormGroup>
             <Label for="email">Email</Label>
-            <Input type="email" name="email" id="email" />
+            <Input type="email" name="email" id="email" value={this.state.email} onChange={this.onEmailChange} />
           </FormGroup>
           <FormGroup>
             <Label for="wallet">Wallet Address</Label>
             <Input plaintext name="wallet" id="wallet">{this.state.wallet}</Input>
           </FormGroup>
         </Form>
-        <Button disabled={this.state.hadTicket} color="primary" onClick={this.onSend}>使用 MetaMask 送出押金</Button>
+        <Button disabled={this.state.hadTicket || !this.state.transaction} color="primary" onClick={this.onSend}>使用 MetaMask 送出押金</Button>
 
+        {this.renderError()}
         {this.renderWarning()}
         {this.renderTransaction()}
       </div>
