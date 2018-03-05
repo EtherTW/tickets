575e729af331fd6b8a4651d9dff1ebfbd6058994
KK Chen <kk@bichenkk.com>
2018-03-05 15:01:25 +0800

    Implemented react-intl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from 'reactstrap';
+import { addLocaleData, IntlProvider } from 'react-intl'
+import enUS from './locales/en-US'
+import zhHANT from './locales/zh-Hant'
 
 import './App.css';
 import Header from './components/Header';
@@ -8,19 +11,40 @@ import Home from './components/Home';
 import Register from './components/Register';
 import Refund from './components/Refund';
 
+addLocaleData(zhHANT.data)
+addLocaleData(enUS.data)
+
 class App extends Component {
+
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  getLocale =() => {
+    const lang = localStorage.getItem('lang')
+    switch (lang) {
+      case 'zh':
+        return zhHANT
+      default:
+        return enUS
+    }
+  }
+
   render() {
+    const locale = this.getLocale()
     return (
-      <Router basename="/tickets">
-        <div className="App">
-          <Header />
-          <Container className="py-3">
-            <Route exact path="/" component={Home} />
-            <Route path="/register" component={Register} />
-            <Route path="/refund" component={Refund} />
-          </Container>
-        </div>
-      </Router>
+      <IntlProvider locale={locale.locale} messages={locale.messages}>
+        <Router basename="/tickets">
+          <div className="App">
+            <Header />
+            <Container className="py-3">
+              <Route exact path="/" component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/refund" component={Refund} />
+            </Container>
+          </div>
+        </Router>
+      </IntlProvider>
     );
   }
 }
---
+++ src/locales/en-US.js
@@ -0,0 +1,10 @@
+import appLocaleData from 'react-intl/locale-data/en'
+import enMessages from './en-US.messages.json'
+
+export default {
+  messages: {
+    ...enMessages,
+  },
+  locale: 'en-US',
+  data: appLocaleData,
+}
