db509c9c8e2cb10d7132bb4db6b6c4e42a9797d7
Yuren Ju <yurenju@gmail.com>
2018-03-07 21:45:37 +0800

    removed refund in contract and added withdraw function

--- contracts/contracts/TEMTicket.sol
+++ contracts/contracts/TEMTicket.sol
@@ -2,70 +2,33 @@ pragma solidity ^0.4.18;
 
 
 contract TEMTicket {
-    uint256 constant public DEPOSIT = 0.03 ether;
-    uint256 constant public MAX_ATTENDEE = 100;
+    uint256 constant public FEE = 0.015 ether;
 
     mapping (uint256 => address) public id2Addr;
     mapping (address => uint256) public userId;
-    address public owner;
     address public TEMWallet;
-    uint256 public attendRecord;
     uint256 public userAmount;
-    bool public emergencyStop;
+    uint256 public maxAttendees;
 
-    function TEMTicket(address _TEMWallet) public {
-        owner = msg.sender;
+    function TEMTicket(address _TEMWallet, uint256 _maxAttendees) public {
         TEMWallet = _TEMWallet;
-        emergencyStop = false;
+        maxAttendees = _maxAttendees;
         userAmount = 0;
     }
 
     function () payable external {
-        if (msg.value == 0) {
-            getRefund();
-  		} else {
-            getTicket(msg.sender);
-  		}
+        getTicket(msg.sender);
     }
 
     function getTicket (address _attendee) payable public {
-        require(msg.value >= DEPOSIT && userId[_attendee] == 0);
+        require(msg.value >= FEE && userId[_attendee] == 0);
         userAmount ++;
-        require(userAmount <= MAX_ATTENDEE);
+        require(userAmount <= maxAttendees);
         userId[_attendee] = userAmount;
         id2Addr[userAmount] = _attendee;
-        TEMWallet.transfer(msg.value - DEPOSIT);
     }
 
-    function getRefund () public {	
-        uint256 index = userId[msg.sender];
-        require(index != 0);
-        require(isAttend(index) || emergencyStop);
-        userId[msg.sender] = 0;
-        msg.sender.transfer(DEPOSIT);
-    } 
-
-    function isAttend (uint256 _index) view returns (bool) {
-        return (16 ** _index) & attendRecord == (16 ** _index);
-    }
-
-    function setAttendBatch (uint256 _attendRecord) external {
-        require(msg.sender == owner);
-        attendRecord = _attendRecord;
-    }
-
-    function setAttendEach (uint256 _index) external {
-        require(msg.sender == owner);
-        attendRecord = attendRecord | (16 ** _index);
-    }
-
-    function transferOwner (address _owner) external {
-        require (msg.sender == owner);
-        owner = _owner;
-    }
-
-    function emergencyStop (bool _stop) external {
-        require(msg.sender == owner);
-        emergencyStop = _stop;
+    function withdraw () public {
+        TEMWallet.transfer(this.balance);
     }
 }
--- contracts/test/test.js
+++ contracts/test/test.js
@@ -6,60 +6,60 @@ contract('test', function(accounts) {
   const user1 = accounts[1]
   const user2 = accounts[2]
   const user3 = accounts[3]
-  const TEMWallet = accounts[4]
-  const DEPOSIT = Number(web3.toWei(0.03, "ether"))
+  const user4 = accounts[4]
+  const TEMWallet = accounts[5]
+  const FEE = Number(web3.toWei(0.03, "ether"))
+
+  const donation1 = 500000;
+  const donation2 = 5000000;
 
   const balanceOf = async function (address) {
     return await web3.eth.getBalance(address)
   }
 
   it("go through all steps", async function() {
-    var ticket = await Ticket.new(TEMWallet, {from: admin})
+    var userAmount;
+    var ticket = await Ticket.new(TEMWallet, 3, {from: admin})
     var ticket_balance = await balanceOf(ticket.address)
 
-    // user1 get the ticket throught fallback function 
-    await web3.eth.sendTransaction({from: user1, to: ticket.address, value: DEPOSIT})
+    userAmount = await ticket.userAmount();
+    assert.equal(userAmount, 0);
 
-    // user2 get the ticket, more than DEPOSIT would be see as donate
-    await ticket.getTicket(user2, {from: user2, value: DEPOSIT + 500000})
+    // user1 get the ticket throught fallback function
+    await web3.eth.sendTransaction({ from: user1, to: ticket.address, value: FEE })
+
+    // user2 get the ticket, more than FEE would be see as donate
+    await ticket.getTicket(user2, { from: user2, value: FEE + donation1})
 
     // user3 get the ticket
-    await web3.eth.sendTransaction({from: user3, to: ticket.address, value: DEPOSIT + 5000000})
+    await web3.eth.sendTransaction({ from: user3, to: ticket.address, value: FEE + donation2})
+    userAmount = await ticket.userAmount();
+    assert.equal(userAmount, 3);
+
 
     // check ticket contract balance
     var ticket_balance2 = await balanceOf(ticket.address)
-    assert.equal(ticket_balance2 - ticket_balance, 3 * DEPOSIT)
+    assert.equal(ticket_balance2 - ticket_balance, 3 * FEE + donation1 + donation2)
 
      // same address should not buy ticket again
     try {
-      await ticket.getTicket(user2, {from: user2, value: DEPOSIT })
+      await ticket.getTicket(user2, {from: user2, value: FEE })
       assert.fail('Expected revert not received')
     } catch (error) {
       const revertFound = error.message.search('revert') >= 0 || error.message.search('assert') >= 0
       assert(revertFound, `Expected "revert", got ${error} instead`)
     }
 
-    // set the attended information 
-    await ticket.setAttendBatch(0x1100, {from: admin}) // user1: 0 user2: 1 user3: 1
-
-    // user1 is not attended, should not get back the fund 
+    // reach max attendees
     try {
-      await ticket.getRefund({from: user1})
+      await ticket.getTicket(user4, { from: user4, value: FEE })
       assert.fail('Expected revert not received')
     } catch (error) {
       const revertFound = error.message.search('revert') >= 0 || error.message.search('assert') >= 0
       assert(revertFound, `Expected "revert", got ${error} instead`)
     }
-    
-    // user2 getRefund throught fallback function 
-    await web3.eth.sendTransaction({from: user2, to: ticket.address})
 
-    // user3 getRefund
-    await ticket.getRefund({from: user3})
-
-    // check ticket contract balance, should only remain user1's deposit 
-    var ticket_balance3 = await balanceOf(ticket.address)
-    console.log(ticket_balance3)
-    assert.equal(ticket_balance3, DEPOSIT)
+    userAmount = await ticket.userAmount();
+    assert.equal(userAmount, 3);
   });
 });
